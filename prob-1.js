e989822f37d911265d4e5916c8fd444ec7169ca2
opalnovae <jnbostow@gmail.com>
2018-10-27 10:17:04 -0700

    Project Euler Problem 1 Multiples of 3 and 5

---
+++ prob-1.js
@@ -0,0 +1,19 @@
+/*If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.*/
+
+
+function multiplesOf3and5(limitNum) {
+    let count = 0, sum = 0;
+    
+    while(count < limitNum) {
+        if( count % 3 === 0 || count % 5 === 0 ) {
+            sum += count;
+        }
+        count++;
+    }
+
+    return sum
+}
+
+console.log(multiplesOf3and5(1000))
